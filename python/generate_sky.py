bfb0a708c649dd2dc1b57b1510bfdaae9d819063
Peter Yoachim <yoachim@uw.edu>
2016-08-17 16:30:37 -0700

    generates files

--- python/generate_sky.py
+++ python/generate_sky.py
@@ -2,13 +2,11 @@ import numpy as np
 import lsst.sims.skybrightness as sb
 import lsst.sims.utils as utils
 import healpy as hp
-import os, sys
-
-# XXX--switch this up to dynamically decide what the timestep should be.  Say, if linear interpolation gets the right answer
-# to within 0.3 mags, then it's good enough? That way we get smaller timesteps at moon rise/set and twilight, longer when stable!
+import sys
 
 
-def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., outfile='generated_sky.npz', nside=32,
+def generate_sky(mjd0=59560.2, duration=0.1, timestep=5., timestep_max=20.,
+                 outfile='generated_sky.npz', nside=32,
                  sunLimit=-12., fieldID=False, airmass_limit=1.5, dm=0.3, verbose=True):
     """
     Use the sky brightness model to generate a number of useful numpy arrays that can be used
@@ -18,25 +16,15 @@ def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., outfile='generated_sk
     sunLimit = np.radians(sunLimit)
 
     # Set the time steps
-    mjds = []
-    mjd_max = mjd0 + duration
+    mjd_max = mjd0 + duration*365.25
     timestep = timestep / 60. / 24.  # Convert to days
-
-    if verbose:
-        print 'generating %i unique MJDs' % mjds.size
-
-    # Maybe add in relevant time points like moon rise/set and twilight times
-
-    # set the RA, Dec array
-    hpindx = np.arange(hp.nside2npix(nside))
-    ra, dec = utils.hpid2RaDec(nside, hpindx)
-
+    timestep_max = timestep_max / 60. / 24.  # Convert to days
     # Switch the indexing to opsim field ID if requested
     if fieldID:
         field_data = np.loadtxt('fieldID.dat', delimiter='|', skiprows=1,
-                                dtype=zip(['id', 'ra', 'dec'],[int, float, float]))
-        ra = np.radians(field_data['ra'])
-        dec = np.radians(field_data['dec'])
+                                dtype=zip(['id', 'ra', 'dec'], [int, float, float]))
+        ra = field_data['ra']
+        dec = field_data['dec']
     else:
         hpindx = np.arange(hp.nside2npix(nside))
         ra, dec = utils.hpid2RaDec(nside, hpindx)
@@ -47,65 +35,82 @@ def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., outfile='generated_sk
     # Set up the sky brightness model
     sm = sb.SkyModel(mags=True)
 
-    sky_properties = {}
     filter_names = ['u', 'g', 'r', 'i', 'z', 'y']
 
-    names = ['sky_brightness'] #, 'nominal_m5']
-    types = [float] * len(names)
-
+    # Initialize the relevant lists
+    dict_of_lists = {'airmass': [], 'sunAlts': [], 'mjds': []}
+    sky_brightness = {}
     for filter_name in filter_names:
-        sky_properties[filter_name] = np.zeros((mjds.size, ra.size), dtype=float)  #  dtype=zip(names, types))
+        sky_brightness[filter_name] = []
 
-    names = ['airmass'] #, 'd2moon', 'mask']
-    types = [float] #, float, int]
-    general_properties = np.zeros((mjds.size, ra.size))  #  , zip(names, types))
-    sunAlts = np.zeros(mjds.size, dtype=float)
-    sunAlts.fill(-666)
-
-    maxI = mjds.size
-    for i, mjd in enumerate(mjds):
+    mjd = mjd0
+    while len(dict_of_lists['mjds']) < 2:
         sm.setRaDecMjd(ra, dec, mjd, degrees=True)
-        sunAlts[i] = sm.sunAlt
         if sm.sunAlt <= sunLimit:
             mags = sm.returnMags()
             for key in filter_names:
-                sky_properties[key][i, :] += mags[key]
-            general_properties[i, :] += sm.airmass
-        progress = i/float(maxI)*100
+                sky_brightness[key].append(mags[key])
+            dict_of_lists['airmass'].append(sm.airmass)
+            dict_of_lists['sunAlts'].append(sm.sunAlt)
+            dict_of_lists['mjds'].append(mjd)
+
+        mjd += timestep
+        if mjd > mjd_max:
+            print 'Failed to get started'
+            return
+    length = mjd_max-mjd0
+    while mjd < mjd_max:
+        progress = (mjd-mjd0)/length*100
         text = "\rprogress = %.1f%%"%progress
         sys.stdout.write(text)
         sys.stdout.flush()
-            # XXX-need to call a haversine here I think
-            #general_properties['d2moon'][:, i] =
-        print ''
+        sm.setRaDecMjd(ra, dec, mjd, degrees=True)
+        if sm.sunAlt <= sunLimit:
+            mags = sm.returnMags()
+            for key in filter_names:
+                sky_brightness[key].append(mags[key])
+            dict_of_lists['airmass'].append(sm.airmass)
+            dict_of_lists['sunAlts'].append(sm.sunAlt)
+            dict_of_lists['mjds'].append(mjd)
 
+            # Check if we can interpolate the second to last sky brightnesses
+            overhead = np.where((dict_of_lists['airmass'][-1] <= airmass_limit) &
+                                (dict_of_lists['airmass'][-2] <= airmass_limit))
+            # XXX -- might need to also excude things near the moon
+            if np.size(overhead[0]) > 0:
+                can_interp = True
+                mjd1 = dict_of_lists['mjds'][-3]
+                mjd2 = dict_of_lists['mjds'][-2]
+                mjd3 = dict_of_lists['mjds'][-1]
+                # Linear interpolation weights
+                w1 = (mjd2-mjd1) / (mjd3-mjd1)
+                w2 = (mjd3-mjd2) / (mjd3-mjd1)
+                for filter_name in filter_names:
+                    interp_sky = w1 * sky_brightness[filter_name][-3][overhead]
+                    interp_sky += w2 * sky_brightness[filter_name][-1][overhead]
+                    diff = np.abs(sky_brightness[filter_name][-2][overhead]-interp_sky)
+                    if np.size(diff[~np.isnan(diff)]) > 0:
+                        if np.max(diff[~np.isnan(diff)]) > dm:
+                            can_interp = False
+                # If the timestep is getting big, don't interp even if you can
+                if (dict_of_lists['mjds'][-3] - dict_of_lists['mjds'][-2]) > timestep_max:
+                    can_interp = False
+                if can_interp:
+                    for key in dict_of_lists:
+                        del dict_of_lists[key][-2]
+                    for key in sky_brightness:
+                        del sky_brightness[key][-2]
+        mjd += timestep
+    print ''
 
-    toSave = np.where(sunAlts <= np.radians(sunLimit))[0]
-    # Collapse down and remove the mjds that had no info
-    for key in filter_names:
-        sky_properties[key][np.isnan(sky_properties[key])] = 0
-        sky_properties[key] = sky_properties[key][toSave, :]
-        sky_properties[key][np.where(sky_properties[key] == 0)] = hp.UNSEEN
+    for key in dict_of_lists:
+        dict_of_lists[key] = np.array(dict_of_lists[key])
+    for key in sky_brightness:
+        sky_brightness[key] = np.array(sky_brightness[key])
 
-    mjds = mjds[toSave]
-    sunAlts = sunAlts[toSave]
-    # XXX--collapse the arrays down and only save those with data
-    maxes = np.zeros((toSave.size, len(filter_names)), dtype=float)
-    for i, filter_name in enumerate(filter_names):
-        maxes[:, i] = np.max(sky_properties[:, i], dimension=1)
-    maxes = np.max(maxes, dimension=1)
-
-    toSave = np.where(maxes > 0)[0]
-    for key in filter_names:
-        sky_properties[key] = sky_properties[key][toSave, :]
-    mjds = mjds[toSave]
-    sunAlts = sunAlts[toSave]
-
-
-
-    np.savez(outfile, general_properties=general_properties,
-             sky_properties=sky_properties, mjds=mjds, sunAlts=sunAlts)
+    np.savez(outfile, dict_of_lists = dict_of_lists, sky_brightness=sky_brightness)
 
 if __name__ == "__main__":
-    generate_sky()
 
+    generate_sky(fieldID=True)
+    #generate_sky()
