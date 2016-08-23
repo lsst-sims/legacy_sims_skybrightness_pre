8b3e8b884c278a0114784bc83d33ba0b863d09a5
Peter Yoachim <yoachim@uw.edu>
2016-08-23 16:26:26 -0700

    adding mask

--- python/generate_sky.py
+++ python/generate_sky.py
@@ -9,7 +9,8 @@ from lsst.sims.skybrightness.utils import mjd2djd
 
 def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., timestep_max=20.,
                  outfile='generated_sky.npz', nside=32,
-                 sunLimit=-12., fieldID=False, airmass_limit=1.5, dm=0.3, verbose=True):
+                 sunLimit=-12., fieldID=False, airmass_overhead=1.5, dm=0.2,
+                 airmass_limit=2.5, moon_dist_limit=30., verbose=True):
     """
     Use the sky brightness model to generate a number of useful numpy arrays that can be used
     to look-up sky brighntess and other pre-computed info
@@ -43,7 +44,6 @@ def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., timestep_max=20.,
 
     mjds = mjds[np.where(sunAlts <= np.radians(sunLimit))]
 
-
     if fieldID:
         field_data = np.loadtxt('fieldID.dat', delimiter='|', skiprows=1,
                                 dtype=zip(['id', 'ra', 'dec'], [int, float, float]))
@@ -63,7 +63,7 @@ def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., timestep_max=20.,
     filter_names = ['u', 'g', 'r', 'i', 'z', 'y']
 
     # Initialize the relevant lists
-    dict_of_lists = {'airmass': [], 'sunAlts': [], 'mjds': []}
+    dict_of_lists = {'airmass': [], 'sunAlts': [], 'mjds': [], 'masks': []}
     sky_brightness = {}
     for filter_name in filter_names:
         sky_brightness[filter_name] = []
@@ -83,13 +83,26 @@ def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., timestep_max=20.,
             dict_of_lists['airmass'].append(sm.airmass)
             dict_of_lists['sunAlts'].append(sm.sunAlt)
             dict_of_lists['mjds'].append(mjd)
-            # XXX don't toss things now.
-            if False:
+
+            mask = np.zeros(np.size(ra), dtype=bool)
+            mask.fill(False)
+            # Apply airmass masking limit
+            mask[np.where((sm.airmass > airmass_limit) | (sm.airmass < 1.))] = True
+
+            # Apply moon distance limit
+            mask[np.where(sm.moonTargSep <= np.radians(moon_dist_limit))] = True
+
+            dict_of_lists['masks'].append(mask)
+
+            if len(dict_of_lists['airmass']) > 3:
                 # Check if we can interpolate the second to last sky brightnesses
-                overhead = np.where((dict_of_lists['airmass'][-1] <= airmass_limit) &
-                                    (dict_of_lists['airmass'][-2] <= airmass_limit))
-                # XXX -- might need to also excude things near the moon
-                if np.size(overhead[0]) > 0:
+                overhead = np.where((dict_of_lists['airmass'][-1] <= airmass_overhead) &
+                                    (dict_of_lists['airmass'][-2] <= airmass_overhead) &
+                                    (~dict_of_lists['masks'][-1]) &
+                                    (~dict_of_lists['masks'][-2]))
+
+                if (np.size(overhead[0]) > 0) & (dict_of_lists['mjds'][-1] -
+                                                 dict_of_lists['mjds'][-3] < timestep_max):
                     can_interp = True
                     mjd1 = dict_of_lists['mjds'][-3]
                     mjd2 = dict_of_lists['mjds'][-2]
@@ -105,9 +118,6 @@ def generate_sky(mjd0=59560.2, duration=0.01, timestep=5., timestep_max=20.,
                         if np.size(diff[~np.isnan(diff)]) > 0:
                             if np.max(diff[~np.isnan(diff)]) > dm:
                                 can_interp = False
-                    # If the timestep is getting big, don't interp even if you can
-                    if (dict_of_lists['mjds'][-3] - dict_of_lists['mjds'][-2]) > timestep_max:
-                        can_interp = False
                     if can_interp:
                         for key in dict_of_lists:
                             del dict_of_lists[key][-2]
