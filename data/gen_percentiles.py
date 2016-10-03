5c1e27f59d610124c406c21515192c93e45b25f2
Peter Yoachim <yoachim@uw.edu>
2016-10-03 16:21:25 -0700

    make percentiles cummulative.

--- data/gen_percentiles.py
+++ data/gen_percentiles.py
@@ -2,7 +2,8 @@ import numpy as np
 from lsst.sims.skybrightness_pre import SkyModelPre, conditions2m5
 from lsst.sims.photUtils import LSSTdefaults
 
-def generate_percentiles(nbins=200):
+
+def generate_percentiles(nbins=20):
     """
     Make histograms of the 5-sigma limiting depths for each point and each filter.
     """
@@ -11,16 +12,12 @@ def generate_percentiles(nbins=200):
     # Number of spatial pixels
     npix = sm.sb['r'].shape[-1]
     mid_mjds = (sm.mjd_left + sm.mjd_right) / 2.
+    # One year should be fine.
+    mid_mjds = [mid_mjds[0]]
     filters = ['u', 'g', 'r', 'i', 'z', 'y']
 
-    bins = np.zeros(nbins+1, dtype=zip(filters, [float]*6))
-    mag_min = 16
-    mag_max = 25
-    for filtername in filters:
-        bins[filtername] = np.linspace(mag_min, mag_max, num=nbins+1)
-
-    histograms = np.zeros((nbins, npix), dtype=zip(filters, [int]*6))
-
+    histograms = np.zeros((nbins, npix), dtype=zip(filters, [float]*6))
+    histogram_npts = np.zeros(npix, dtype=zip(filters, [int]*6))
     timestep = 10.  # minutes
     timestep = timestep / 60. / 24.
 
@@ -49,15 +46,23 @@ def generate_percentiles(nbins=200):
             FWHMeff = LSSTdefaults().FWHMeff(filtername)
             sm.sb[filtername] = conditions2m5(FWHMeff, sm.sb[filtername],
                                               sm.info['airmass'], filtername=filtername)
+
             for indx in np.arange(npix):
                 # Toss out mjds that should be masked
                 # mjds = all_mjds[sm.info['masks'][mjd_indxes, indx]]
+                # Compute the mjds at the correct times
                 m5s = np.interp(mjds, sm.info['mjds'], sm.sb[filtername][:, indx])
+                m5s = m5s[np.isfinite(m5s)]
+                m5s = np.sort(m5s)
+                percentile_points = np.round(np.linspace(0, m5s.size-1, nbins))
+                if m5s.size > percentile_points.size:
+                    histograms[filtername][:, indx] = m5s[percentile_points.astype(int)]
+                    histogram_npts[filtername][indx] = m5s.size
                 # make the histogram for this point in the sky
-                histograms[filtername][:, indx] += np.histogram(m5s[np.isfinite(m5s)],
-                                                                bins=bins[filtername])[0]
+                # histograms[filtername][:, indx] += np.histogram(m5s[np.isfinite(m5s)],
+                #                                                bins=bins[filtername])[0]
 
-    np.savez('percentile_m5_maps.npz', histograms=histograms, bins=bins)
+    np.savez('percentile_m5_maps.npz', histograms=histograms, histogram_npts=histogram_npts)
 
 
 if __name__ == '__main__':
