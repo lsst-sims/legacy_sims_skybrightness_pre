e5b5aba15d15aeb54cc9cb7db5248e28d076cc0a
Peter Yoachim <yoachim@uw.edu>
2017-03-13 16:54:30 -0700

    blah

--- python/lsst/sims/skybrightness_pre/SkyModelPre.py
+++ python/lsst/sims/skybrightness_pre/SkyModelPre.py
@@ -85,8 +85,9 @@ class SkyModelPre(object):
 
         if not self.opsimFields:
             self.nside = hp.npix2nside(self.sb[self.filter_names[0]][0, :].size)
-        self.loaded_range = np.array([self.mjd_left[file_indx], self.mjd_right[file_indx]])
-        if filename is not None:
+        if filename is None:
+            self.loaded_range = np.array([self.mjd_left[file_indx], self.mjd_right[file_indx]])
+        else:
             self.loaded_range = np.array([self.info['mjds'].min(), self.info['mjds'].max()])
 
     def returnSunMoon(self, mjd):
