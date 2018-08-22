61e02d12a101c9044ebfb936517519cf0df9c7d6
R. Biswas <rbiswas4@gmail.com>
2018-08-22 08:13:34 -0700

    Added the same -z flag for `data_up.sh` but have not checked this helps 	modified:   data/data_up.sh

--- data/data_up.sh
+++ data/data_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Copy data up to NCSA
-rsync -av --progress healpix/*.npz* lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/healpix/"
-rsync -av --progress healpix/*.npy* lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/healpix/"
-rsync -av --progress opsimFields/*.npz lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/opsimFields/"
-rsync -av --progress opsimFields/*.npy lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/opsimFields/"
-rsync -av --progress percentile_m5_maps.npz lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/"
+rsync -avz --progress healpix/*.npz* lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/healpix/"
+rsync -avz --progress healpix/*.npy* lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/healpix/"
+rsync -avz --progress opsimFields/*.npz lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/opsimFields/"
+rsync -avz --progress opsimFields/*.npy lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/opsimFields/"
+rsync -avz --progress percentile_m5_maps.npz lsst-dev01.ncsa.illinois.edu:"/datasets/public_html/sim-data/sims_skybrightness_pre/"
